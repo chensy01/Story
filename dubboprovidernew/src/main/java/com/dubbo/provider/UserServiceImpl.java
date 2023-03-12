9cd7d0746440cc64921a181f1ae78f861162fee3
chensy01 <csy.mailbox@qq.com>
2023-03-12 14:43:33 +0800

    dubbo test

---
+++ dubboprovidernew/src/main/java/com/dubbo/provider/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.dubbo.provider;
+
+import com.dubbo.api.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String sayHi(String s){
+        return "hello " + s + "!";
+    }
+
+}
